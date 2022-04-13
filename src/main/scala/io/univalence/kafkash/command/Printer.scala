d15a01d5ba93c23e56cc89367f3c74c79bddbb64
François Sarradin <fsarradin@gmail.com>
2022-04-13 15:08:40 +0200

    remove v0.1

--- src/main/scala/io/univalence/kafkash/command/Printer.scala
+++
@@ -1,6 +0,0 @@
-package io.univalence.kafkash.command
-
-object Printer {
-  def print(x: Any): Unit                = Console.println(x)
-  def print(color: String, x: Any): Unit = Console.println(color + x.toString + Console.RESET)
-}
